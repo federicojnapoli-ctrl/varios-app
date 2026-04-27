1ae59e19604380c7e81200580f3231f8d5157455
federicojnapoli-ctrl <federicojnapoli@gmail.com>
2026-04-27 15:16:00 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,31 @@
-const CACHE = 'varios-v2';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'varios-v3';
+const ASSETS = [
+  '/varios-app/',
+  '/varios-app/index.html',
+  '/varios-app/manifest.json',
+  '/varios-app/icon-192.png',
+  '/varios-app/icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    fetch(e.request)
+      .catch(() => caches.match(e.request))
   );
 });
